d5c605c08c3a01b361e799d9e7309d13f85bb2f7
kevin-salamon <kevinwebdevs@gmail.com>
2020-03-03 14:07:17 -0500

    added potential routing for adding appetizers

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,37 +4,26 @@ var db = require("../models");
 module.exports = function(app) {
   app.post("/", function(req, res) {
     db.Diningroom.create({
-      seats: req.body.totalSeats,
+      seats: req.body
     }).then(function(dbDiningroom) {
         res.render("index", dbDiningroom);
     });
   });
 
-  app.put("/appetizer", function(req, res) {
-    db.tablehistory.update({
-        app_time: req.body.appetizerOrdered,
-    }, {
-        where: {
 
+  app.post("/check-in", function(req, res) {
+    db.Tablehistory.create(req.body);
+  });
+
+  app.put("/availbility", function(req, res) {
+    db.Diningroom.update(req.body.availability, {
+        where: {
+            id: req.body.id
         }
-    }).then(function(dbTableHistory) {
-        res.render("index", dbTableHistory);
+    }).then(function(dbDiningroom) {
+        res.render('index', dbDiningroom);
     });
   });
+
 };
 
-
-// app.put("/api/todos", function(req, res) {
-//     // Update takes in an object describing the properties we want to update, and
-//     // we use where to describe which objects we want to update
-//     db.Todo.update({
-//       text: req.body.text,
-//       complete: req.body.complete
-//     }, {
-//       where: {
-//         id: req.body.id
-//       }
-//     }).then(function(dbTodo) {
-//       res.json(dbTodo);
-//     });
-//   });
