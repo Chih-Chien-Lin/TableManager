245bbed7e51f836a8273177b6e6dbc7d4bc182cd
Kenny Lam <kinwai.lam730@gmail.com>
2020-03-03 22:16:19 -0500

    Worked on the table and api-route js, added comments to most of what I did or the logic behind it

--- public/js/table.js
+++ public/js/table.js
@@ -4,7 +4,7 @@ $(function() {
         //display of model DOM === "block"
     });
 
-    $("#create-table").on("submit", function(event) {
+    $("#create-table").on("submit", function(event) { //this creates a table in the diningroom.js file
         event.preventDefault();
         var totalSeats = $("#table-count").val().trim();
         totalSeats = parseInt(tableCount);
@@ -17,7 +17,7 @@ $(function() {
             data: seats
           }).then(function() {
               console.log("New Table Added!");
-              location.reload();
+              location.reload();  
             }
           );
 
@@ -31,52 +31,82 @@ $(function() {
         //display of pay DOM = "none"
     });
 
-    $(".check-in").on("submit", function(event) {
+    // the class check-in is the appetizer button, this is to guarentee the customer has placed an order and can generate a table id, otherwise if a customer walks out you don't have a "dead" table history.
+    $(".check-in").on("submit",function(event){ 
         event.preventDefault();
-
         var id = $(this).data("id");
-
-        var time = {
-            start_at: moment.format('LTS'),
-            table_color: "green"
-        }
-        $.ajax("/check-in", {
-            type: 'POST',
-            data: time
-        }).then(function(response) {
-            if (response !== null) {
-                var availability = {
-                    availability: false,
-                    id: id
-                };
-
-                $.ajax("/availability", {
-                    type: "PUT",
-                    data: availability
-                }).then(function(res) {
-                    location.reload();
-                });
+        //this creates a JSON which will turn the table avaibility false and the id is used to tell it which table is now not availible
+        var availability = { 
+            availability: false,
+            id: id
+        };
+        $.ajax("/availability", {
+            type: "PUT",
+            data: availability
+        }).then(function(res) {
+            //time will grab the time from moment and change the table_color to green, so when it renders on a reload it'll be green.
+            var time = { 
+                start_at: moment.format('LTS'),
+                table_color: "green"
             }
+            $.ajax("/check-in", {
+                type: 'POST',
+                data: time
+            }).then(function(){
+                location.reload()
+            })
         });
-    });
+    })
+
+    // $(".check-in").on("submit", function(event) {  <--- same function as above but reversed the order
+    //     event.preventDefault();
+    //     var id = $(this).data("id");
+    //     var time = {
+    //         start_at: moment.format('LTS'),
+    //         table_color: "green"
+    //     }
+    //     $.ajax("/check-in", {
+    //         type: 'POST',
+    //         data: time
+    //     }).then(function(response) {
+    //         if (response !== null) {
+    //             var availability = {
+    //                 availability: false,
+    //                 id: id
+    //             };
+
+    //             $.ajax("/availability", {
+    //                 type: "PUT",
+    //                 data: availability
+    //             }).then(function(res) {
+    //                 location.reload();
+    //             });
+    //         }
+    //     });
+    // });
 
     $(".entree").on("submit", function(event){
         event.preventDefault();
+        let tableId = $(this).data("tableId")
         let tableColor = {
-            table_color: "Yellow"
+            table_color: "Yellow",
+            id: tableId
         }
         $.ajax("/entree",{
             type: "PUT",
             data: tableColor
         }).then(function(){
             console.log("Entree has been served!")
+            location.reload()
         })
     })
 
     $(".dessert").on("submit", function(event){
         event.preventDefault();
+        let tableId = $(this).data("tableId")
         let tableColor = {
-            table_color: "Red"
+            table_color: "Red",
+            id: tableId
         }
         $.ajax("/entree",{
             type: "PUT",
@@ -88,6 +118,7 @@ $(function() {
     // $(".entree")
     $(".clear").on("submit", function(event){
         event.preventDefault()
+        let tableId =$(this).data("tableId");
         let id = $(this).data("id");
         let availability = true;
 
@@ -97,17 +128,19 @@ $(function() {
         }
         $.ajax("/availability", {
             type: "PUT",
-            data: availability
+            data: update
         }).then(function(){
             let clearTable = {
                 table_color: white,
-                end_at: moment.format('LTS')
+                end_at: moment.format('LTS'),
+                id: tableId
             };
             $.ajax("/clear",{
                 type: "PUT",
                 data: clearTable
             }).then(function(dbClear){
-                res.render("Index", dbClear)
+                // res.render("Index", dbClear) <-- think render goes in the route folder but either way I think it automatically renders when the page reloads
+                location.reload()
             })
         })
     })
