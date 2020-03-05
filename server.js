0a951c0ac2fa754cdfb42fa1c855bd0d303c6723
Justin Lin <ro0566320@gmail.com>
2020-03-05 09:53:10 -0500

    minor change for desk setup

--- server.js
+++ server.js
@@ -14,4 +14,4 @@ db.sequelize.sync({ force: true }).then(function() {
     app.listen(PORT, function() {
       console.log("App listening on PORT " + PORT);
     });
-  });
+  });                      
